244b505b1777ebfe20acc9a932d240342a6a60d5
Rajan Vishwakarma <116245088+rajan12345vishwakarma@users.noreply.github.com>
2023-09-29 22:41:13 +0530

    Create GenericClass.cs

---
+++ GenericClass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Generics<T>
+    {
+        public T Sum(int a, int b)
+        {
+            dynamic d1 = a; dynamic d2 = b;
+            return (d1 + d2);
+        }
+    }
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Generics<int> g = new Generics<int>();
+            g.Sum(10, 20);
+            Console.WriteLine(g.Sum(1, 2));
+            Console.ReadLine();
+        }
+    }
+}
