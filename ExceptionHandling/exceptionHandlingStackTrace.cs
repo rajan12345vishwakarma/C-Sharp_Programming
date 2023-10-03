9fba0170eeb08a68cb8354e999c173fd68d92248
Rajan Vishwakarma <116245088+rajan12345vishwakarma@users.noreply.github.com>
2023-10-03 19:12:31 +0530

    Create exceptionHandlingStackTrace.cs

---
+++ ExceptionHandling/exceptionHandlingStackTrace.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practice
+{
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message { get { return "Attempt to divide by odd number"; } }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                test1();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+               
+            }
+           
+            Console.ReadLine();
+        }
+        public static void test1()
+        {
+            try
+            {
+                test2();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public static void test2()
+        {
+            try
+            {
+                test3();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public static void test3()
+        {
+            try
+            {
+                int x = 10;
+                int y = 0;
+                int res = x / y;
+                Console.WriteLine(res);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
+
+/*
+ if we throw ex then it stops stack trace and if we don't throw ex then 
+ it print all the stack trace if any error comes.
+ */
