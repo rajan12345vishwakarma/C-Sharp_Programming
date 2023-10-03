7d6d8aa8af9a7e297dc24392b1a67948505e0305
Rajan Vishwakarma <116245088+rajan12345vishwakarma@users.noreply.github.com>
2023-10-03 18:58:27 +0530

    Create applicationError.cs

---
+++ ExceptionHandling/applicationError.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Practice
+{
+    public class DivideByOddNoException : ApplicationException
+    {
+        public override string Message { get { return "Attempt to divide by odd number"; } }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter first number");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ener second number");
+                int y = int.Parse(Console.ReadLine());
+                if (y % 2 > 0)
+                {
+                   // throw new ApplicationException("divisor value can't be odd number");
+                    throw new DivideByOddNoException();
+                }
+                int res = x % y;
+                Console.WriteLine($"result = {x / y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+               
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
