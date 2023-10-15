4dd32964d14bafa55492bd8d84ce285d4fb03e87
Rajan Vishwakarma <116245088+rajan12345vishwakarma@users.noreply.github.com>
2023-10-15 21:27:49 +0530

    Update applicationError.cs

--- ExceptionHandling/applicationError.cs
+++ ExceptionHandling/applicationError.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
 namespace Practice
 {
     public class DivideByOddNoException : ApplicationException
