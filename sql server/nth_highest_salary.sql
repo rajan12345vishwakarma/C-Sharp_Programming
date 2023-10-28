5900ef625e2d4bd3cd4f365aea32d83b0ac5e3d2
Rajan Vishwakarma <116245088+rajan12345vishwakarma@users.noreply.github.com>
2023-10-28 23:50:39 +0530

    Update nth_highest_salary.sql

--- sql server/nth_highest_salary.sql
+++ sql server/nth_highest_salary.sql
@@ -1,4 +1,5 @@
-		SELECT * FROM Worker
+// first method
+SELECT * FROM Worker
 SELECT * FROM Worker WHERE SALARY = 
          (
             SELECT MIN(SALARY) FROM Worker 
@@ -7,3 +8,12 @@ SELECT * FROM Worker WHERE SALARY =
             SALARY FROM Worker 
             ORDER BY SALARY DESC
 ))
+
+// second method
+SELECT *
+FROM Worker W1
+WHERE 3 = (
+ SELECT COUNT( DISTINCT ( W2.Salary ) )
+ FROM Worker W2
+ WHERE W2.Salary >= W1.Salary
+ );
